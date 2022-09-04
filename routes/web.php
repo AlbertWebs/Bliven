27ade05dc1a8069b2b66e1815425f50f07e26b7a
Albert Muhatia <albertmuhatia@gmail.com>
2022-09-04 13:27:40 +0300

    Update Auth Module

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('front.index');
 });
+
+Auth::routes();
+Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
